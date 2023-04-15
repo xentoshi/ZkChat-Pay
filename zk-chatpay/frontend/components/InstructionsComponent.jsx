855360b52568bcfbee4b944ec987228c6f54e21b
xentoshi <xentoshi@protonmail.com>
2023-04-15 12:10:46 +0900

    Landing page changes

--- zk-chatpay/frontend/components/InstructionsComponent.jsx
+++ zk-chatpay/frontend/components/InstructionsComponent.jsx
@@ -1,16 +1,21 @@
 import styles from "../styles/InstructionsComponent.module.css";
 import Router, { useRouter } from "next/router";
+
+const plant = "https://giphy.com/embed/ZzuUZRMcRv7oRtV79K"
+
 export default function InstructionsComponent() {
 	const router = useRouter();
 	return (
 		<div className={styles.container}>
 			<header className={styles.header_container}>
 				<h1>
-					zk-chat
+				Become a planetary gardener
 				</h1>
-			</header>
+				<h2> Our Lovely, Lovely World, a platform to directly & anonymously donate to reforestation projects </h2>
+				<iframe src={plant} width="250" height="250" frameBorder="0" title="plant" className='plant' allowFullScreen></iframe>
 
-			
+				<button>launch marketplace</button>
+			</header>
 		</div>
 	);
 }
